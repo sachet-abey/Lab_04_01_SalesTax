f776f5032f3bbfbc25d4e2144ab8e953ca961f4e
sachet-abey <sachet.abeysinghe29@ihsd.us>
2024-02-02 20:53:24 -0500

    Lab 04 Sales Tax Project Task 1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double purchasePrice = 15.50; // Initialize double variable for purchase price
+        double salesTax = purchasePrice * 5/100; // Initialize sales tax variable by multiplying 5% to purchase price
+        System.out.println("The 5% sales tax for the price $"+purchasePrice+" is $"+salesTax); // Print results
+    }
+}
